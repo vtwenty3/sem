09f33b7b90317d1598ae8caa317c3e34bf01fec7
Valentin Kisimov <kisimovvalentin@gmail.com>
2021-02-09 14:15:12 +0000

    built project into a single JAR

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -10,13 +10,14 @@ public class App
     public static void main(String[] args)
     {
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
+        Document doc = new Document("name", "Valentin K.")
                 .append("class", "Software Engineering Methods")
                 .append("year", "2021")
                 .append("result", new Document("CW", 95).append("EX", 85));
